95e3a87aac98268b39e0d896f7a818ba85ee094e
SyedSibtainRazvi <Syedsibtain191@gmail.com>
2022-12-29 23:31:05 +0530

    added animation for modal

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,35 +1,31 @@
+import { Menu, Transition } from '@headlessui/react'
 import React from 'react'
-import { Menu } from '@headlessui/react'
 
 function MyDropdown() {
   return (
     <Menu>
-      <Menu.Button>More</Menu.Button>
-      <Menu.Items>
-        <Menu.Item>
-          {({ active }) => (
-            <a
-              className={`${active && 'bg-blue-500'}`}
-              href="/account-settings"
-            >
-              Account settings
-            </a>
-          )}
-        </Menu.Item>
-        <Menu.Item>
-          {({ active }) => (
-            <a
-              className={`${active && 'bg-blue-500'}`}
-              href="/account-settings"
-            >
-              Documentation
-            </a>
-          )}
-        </Menu.Item>
-        <Menu.Item disabled>
-          <span className="opacity-75">Invite a friend (coming soon!)</span>
-        </Menu.Item>
-      </Menu.Items>
+      {({ open }) => (
+        <>
+          <Menu.Button>More</Menu.Button>
+
+          {/* Use the `Transition` component. */}
+          <Transition
+            show={open}
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+          >
+            {/* Mark this component as `static` */}
+            <Menu.Items static>
+              <Menu.Item>{/* ... */}</Menu.Item>
+              {/* ... */}
+            </Menu.Items>
+          </Transition>
+        </>
+      )}
     </Menu>
   )
 }
